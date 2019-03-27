fcb1efc6e20ab51d793b526fa76a2a3721418826
Seidel <database.vwc@gmail.com>
2019-03-26 21:30:16 -0300

    BasicAuthenticationFilter impl

--- src/main/java/me/spark/HelloWorld.java
+++ src/main/java/me/spark/HelloWorld.java
@@ -1,12 +1,21 @@
 package me.spark;
 
+import me.spark.authentication.AuthenticationDetails;
+import me.spark.authentication.BasicAuthenticationFilter;
 import me.spark.user.UserController;
 
+import static spark.Spark.before;
 import static spark.Spark.port;
 
 public class HelloWorld {
     public static void main(String[] args) {
         port(4567);
+
+        before(
+                new BasicAuthenticationFilter("/path/*",
+                        new AuthenticationDetails("expected-username", "expected-password"))
+        );
+
 
         UserController userController = new UserController();
     }
